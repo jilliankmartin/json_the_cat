aef387d4566dd5042c279c6bc27a67332d28c72c
jilliankmartin <jillmartin@live.com>
2020-09-24 20:08:00 +0000

    linted

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-let userInput = process.argv.splice(2)
+let userInput = process.argv.splice(2);
 
 const storeBody = (body, input) => {
   const data = JSON.parse(body);
@@ -8,14 +8,12 @@ const storeBody = (body, input) => {
     return data[0].description;
   }
   return `sorry, ${input} does not exist`;
-  
-}
+};
 
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${userInput[0]}`, (error, response, body) => {
-    if (!error) console.log(storeBody(body, userInput[0]));
-    if (error) console.log(`There was an error. Response: ${error}`)
-    
-  });
+request(`https://api.thecatapi.com/v1/breeds/search?q=${userInput[0]}`, (error, response, body) => {
+  if (!error) console.log(storeBody(body, userInput[0]));
+  if (error) console.log(`There was an error. Response: ${error}`);
+});
 
 
 
