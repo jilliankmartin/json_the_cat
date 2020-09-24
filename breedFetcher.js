7ebfe0d423d1df39e9803e781447a5f226c43e98
jilliankmartin <jillmartin@live.com>
2020-09-24 20:05:52 +0000

    handled errors

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,16 +2,20 @@ const request = require('request');
 
 let userInput = process.argv.splice(2)
 
-const getSiberian = (callback) => {
-  request('https://api.thecatapi.com/v1/breeds/search?q=siberian', (error, response, body) => {
-    callback(body);
-  });
-};
-
-const storeBody = (body) => {
+const storeBody = (body, input) => {
   const data = JSON.parse(body);
-  // console.log(data);
-  // console.log(typeof data);
-  console.log(data[0].description);
+  if (data[0]) {
+    return data[0].description;
+  }
+  return `sorry, ${input} does not exist`;
+  
 }
-console.log(getSiberian(storeBody));
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${userInput[0]}`, (error, response, body) => {
+    if (!error) console.log(storeBody(body, userInput[0]));
+    if (error) console.log(`There was an error. Response: ${error}`)
+    
+  });
+
+
+
